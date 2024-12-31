d35b7856ee1dd07d21a8dd8eb353790502deec96
Adrian Jurado <adrianisrael.616@gmail.com>
2024-12-31 09:08:41 -0500

    Styles fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ import gluestackPlugin from '@gluestack-ui/nativewind-utils/tailwind-plugin';
 module.exports = {
   darkMode: process.env.DARK_MODE ? process.env.DARK_MODE : 'media',
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './src/core-components/**/**/*.{html,js,jsx,ts,tsx}',
-    './src/components/**/*.{html,js,jsx,ts,tsx,mdx}',
-    './src/hooks/**/*.{html,js,jsx,ts,tsx,mdx}',
+    './**/*.{html,js,jsx,ts,tsx}',
+    './core-components/**/**/*.{html,js,jsx,ts,tsx}',
+    './components/**/*.{html,js,jsx,ts,tsx,mdx}',
+    './hooks/**/*.{html,js,jsx,ts,tsx,mdx}',
   ],
   presets: [require('nativewind/preset')],
   theme: {
